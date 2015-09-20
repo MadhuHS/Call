3b6c39a9860645708c205bb8daf370f57bec3e92
MadhuHS <madhuhs943@gmail.com>
2015-09-20 15:36:18 +0530

    new
    commit changes

---
+++ CallApp/QCaller/src/com/example/qcaller/CallListActivity.java
@@ -0,0 +1,16 @@
+package com.example.qcaller;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CallListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_call_list);
+	}
+
+}
